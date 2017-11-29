def4ee2a8da5e3d563661eccf34f70ab885ec4ba
SimonThrane <minijordan23-virum@hotmail.com>
2017-11-29 23:37:57 +0100

    Worked a bit on the LobbyAdapter next step is firebase database

--- PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/Adapters/LobbyAdapter.java
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/Adapters/LobbyAdapter.java
@@ -1,24 +1,49 @@
 package com.thrane.simon.passthebomb.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.thrane.simon.passthebomb.Models.Game;
+import com.thrane.simon.passthebomb.R;
+
+import java.util.List;
 
 /**
  * Created by Simon on 29-11-2017.
  */
 //Highly inspired by Kasper Løvborgs solution for Adapter exercise
 public class LobbyAdapter extends BaseAdapter {
+    private FirebaseDatabase database;
+    public List<Game> gameDataList;
+    private Context context;
+    private Game gameDataItem;
+
+
+    public LobbyAdapter(FirebaseDatabase database) {
+        this.database = database;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        if(gameDataList != null){
+            return gameDataList.size();
+        }else{
+            return 0;
+        }
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
-    }
+    public Game getItem(int position) {
+        if(gameDataList != null) {
+            return gameDataList.get(position);
+        } else {
+            return null;
+        }    }
 
     @Override
     public long getItemId(int i) {
@@ -26,7 +51,27 @@ public class LobbyAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //we only need to create the views once, if not null we will reuse the existing view and update its values
+        if (convertView == null) {
+            LayoutInflater weatherInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = weatherInflater.inflate(R.layout.lobby_list_item, null);
+        }
+
+        gameDataItem = getItem(position);
+        if(gameDataItem!=null){
+            //set the properties from weatherDataItem
+            TextView txtLobbyName = convertView.findViewById(R.id.tvLobbyName);
+            txtLobbyName.setText(gameDataItem.Name);
+
+            TextView txtHostName = convertView.findViewById(R.id.tvHostName);
+            //txtHostName.setText(Integer.toString(gameDataItem.Host.Name));
+
+            TextView txtPlayerNum = convertView.findViewById(R.id.tvPlayerNum);
+            txtPlayerNum.setText(Double.toString(gameDataItem.Users.size()));
+
+        }
+        return convertView;
     }
 }

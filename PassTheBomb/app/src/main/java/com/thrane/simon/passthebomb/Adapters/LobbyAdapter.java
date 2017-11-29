7d49b4ff067dc4412519c66a493d7add4325e96a
SimonThrane <minijordan23-virum@hotmail.com>
2017-11-29 23:19:11 +0100

    Changed LoginActivity to be the first one and added lolly_item and adapter

---
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/Adapters/LobbyAdapter.java
@@ -0,0 +1,32 @@
+package com.thrane.simon.passthebomb.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Simon on 29-11-2017.
+ */
+//Highly inspired by Kasper Løvborgs solution for Adapter exercise
+public class LobbyAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}

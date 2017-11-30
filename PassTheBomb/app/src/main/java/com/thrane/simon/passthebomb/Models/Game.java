4082ecce195e7207b660cf9cda93598592ac9545
holmeren <soren_kh@hotmail.com>
2017-11-30 13:00:05 +0100

    give classes porperties

--- PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/Models/Game.java
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/Models/Game.java
@@ -1,8 +1,15 @@
 package com.thrane.simon.passthebomb.Models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Søren on 26-11-2017.
  */
 
 public class Game {
+    public String password;
+    public boolean isPrivate;
+    public ArrayList<User> players;
+
+
 }

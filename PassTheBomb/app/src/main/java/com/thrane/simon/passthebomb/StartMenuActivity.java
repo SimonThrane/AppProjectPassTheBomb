40e6c63c24a0abc9e51402062c1fba1004933d73
SimonThrane <minijordan23-virum@hotmail.com>
2017-11-30 21:41:26 +0100

    Minor changes in StartMenuActivity.java

--- PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/StartMenuActivity.java
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/StartMenuActivity.java
@@ -1,17 +1,23 @@
 package com.thrane.simon.passthebomb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.thrane.simon.passthebomb.Models.Game;
 
 //Inspired by https://github.com/firebase/FirebaseUI-Android/blob/master/app/src/main/java/com/firebase/uidemo/database/realtime/RealtimeDbChatActivity.java
@@ -21,6 +27,9 @@ public class StartMenuActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference mRef;
     private FirebaseListOptions<Game> options;
+    private Game selectedGame;
+    private Button createLobbyBtn;
+    private Button joinLobbyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +37,42 @@ public class StartMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lobby_list);
         database = FirebaseDatabase.getInstance();
         mRef = database.getReference("Games");
-        Query query = mRef.limitToLast(50);
+        Query query = mRef.limitToLast(5);
         options = new FirebaseListOptions.Builder<Game>()
                 .setLayout(R.layout.lobby_list_item)
                 .setQuery(query, Game.class)
                 .build();
-        //
+
+        createLobbyBtn = findViewById(R.id.createBtn);
+        joinLobbyBtn = findViewById(R.id.joinLobbyBtn);
+
+        createLobbyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent CreateLobbyIntent = new Intent(getBaseContext(), CreateLobbyActivity.class);
+                startActivity(CreateLobbyIntent);
+            }
+        });
+        joinLobbyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(selectedGame != null){
+                    Intent LobbyIntent = new Intent(getBaseContext(), LobbyActivity.class);
+                    startActivity(LobbyIntent);
+                }
+            }
+        });
         gameAdapter = new FirebaseListAdapter<Game>(options) {
             @Override
             protected void populateView(View v, Game gameDataItem, int position) {
                 TextView txtLobbyName = v.findViewById(R.id.tvLobbyName);
-                txtLobbyName.setText(gameDataItem.Name);
+                txtLobbyName.setText(gameDataItem.name);
 
-                //TextView txtHostName = v.findViewById(R.id.tvHostName);
-                //txtHostName.setText(Integer.toString(gameDataItem.Host.Name));
+                TextView txtHostName = v.findViewById(R.id.tvHostName);
+                //txtHostName.setText(Integer.toString(gameDataItem.Host));
 
                 TextView txtPlayerNum = v.findViewById(R.id.tvPlayerNum);
-                txtPlayerNum.setText(Double.toString(gameDataItem.Users.size()));
+                txtPlayerNum.setText(Double.toString(gameDataItem.users.size()));
 
             }
         };
@@ -57,10 +85,26 @@ public class StartMenuActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3)
             {
-                String Id = (String)adapter.getItemAtPosition(position);
-                // assuming string and if you want to get the value on click of list item
-                // do what you intend to do on click of listview row
+                selectedGame = (Game) adapter.getItemAtPosition(position);
             }
         });
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Game value = dataSnapshot.getValue(Game.class);
+                Log.d(TAG, "Value is: " + value.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
 }

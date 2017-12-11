f7d5dd55faece3835b669985dbdb609fff47ffe3
DESKTOP-RPQCK8V\Jeppe <jeppebenjaminsen@hotmail.com>
2017-12-11 11:50:26 +0100

    Påbegyndt resultactivity - kan bruges men har ingen knapper

--- PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/ResultActivity.java
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/ResultActivity.java
@@ -1,13 +1,27 @@
 package com.thrane.simon.passthebomb;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.thrane.simon.passthebomb.Util.Globals;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView txtLoser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        txtLoser = findViewById(R.id.txtLoser);
+
+        Intent intent = getIntent();
+        String loserName = intent.getStringExtra(Globals.LOSER);
+        loserName = "Bobby";
+
+        txtLoser.setText(loserName + " " + getString(R.string.result_loser_description));
     }
 }

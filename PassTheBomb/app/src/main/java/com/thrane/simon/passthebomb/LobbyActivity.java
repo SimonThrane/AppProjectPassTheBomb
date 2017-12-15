1ee42e2fa83eec4500de036fc107c963894e47f0
Simon Thrane <sthranehansen@gmail.com>
2017-12-15 12:23:33 +0100

    Changed behavior to not remove event on query

--- PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/LobbyActivity.java
+++ PassTheBomb/app/src/main/java/com/thrane/simon/passthebomb/LobbyActivity.java
@@ -212,7 +212,6 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         gamesRef.removeEventListener(gameStartedListener);
-        query.removeEventListener(leaveLobbyListener);
         gamesRef.removeEventListener(gameKeyListener);
         gamesRef.removeEventListener(gameListener);
     }
